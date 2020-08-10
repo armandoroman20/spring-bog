4136939d98d2c03043f0633c9cac8af74d1ebc4c
Armando Roman <armando.roman.jr20@gmail.com>
2020-08-10 15:49:26 -0500

    commiting

--- src/main/java/com/codeup/springblog/repositories/UserDetailsLoader.java
+++
@@ -1,7 +0,0 @@
-//package com.codeup.springblog.repositories;
-//
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public interface UserDetailsLoader implements UserDetailsService{
-//}
